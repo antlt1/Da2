fcaa39437c1631c76b83c224624fe4268e923ecb
tranchidat2569 <81204538+tranchidat2569@users.noreply.github.com>
2021-11-19 18:24:32 +0700

    done

--- App_ThuVien/Class/Getting_UI.cs
+++ App_ThuVien/Class/Getting_UI.cs
@@ -46,10 +46,10 @@ namespace Test_Sqlite.Class
         {
            string   s1 =   "datasource=https://databases-auth.000webhost.com,port:3306,username=id15353028_admin,password=Antlt1182000@gmail.com,database=id15353028_db_qltv_anz",
                     s2 = "Server=localhost;Database=thuvien;port=;User Id=root;password=",
-                    s3 = "datasource=26.9.216.208:8080;Database=thuvien;port=;User Id=root;password=";
+                    s3 = "Server=26.9.216.208;Database=thuvien;port=;User Id=root;password=";
 
             string str = s2,str1=s1;
-            MySqlConnection conn = new MySqlConnection(s3);
+            MySqlConnection conn = new MySqlConnection(s2);
             return conn;
         }
         // login
